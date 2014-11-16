c099beec561e0fec4a9323f37414247f766faf2a
Irina Rakhlenko <rakhlei@miamioh.edu>
2014-11-16 18:37:24 -0500

    Add DoRandomFindMethod

--- workspace/rakhlei_hw_5/src/DataStructureTester.h
+++ workspace/rakhlei_hw_5/src/DataStructureTester.h
@@ -57,7 +57,7 @@ int DataStructureTester<L>::DoRandomAdd(int n) {
 	//srand(0);
 	t_ = clock();
 	for (int i = 0; i < n; i++) {
-		int x = rand()%100000;
+		int x = rand() % 100000;
 		list.add(x);
 	}
 	t_ = clock() - t_;
@@ -68,11 +68,11 @@ int DataStructureTester<L>::DoRandomAdd(int n) {
 template<class L>
 int DataStructureTester<L>::DoSequentialRemove(int start, int end, int step) {
 	t_ = clock();
-		for (int i = start; i <= end; i += step) {
-			list.remove(i);
-		}
-		t_ = clock() - t_;
-		return (float) t_ / CLOCKS_PER_SEC * 1000;
+	for (int i = start; i <= end; i += step) {
+		list.remove(i);
+	}
+	t_ = clock() - t_;
+	return (float) t_ / CLOCKS_PER_SEC * 1000;
 }
 
 template<class L>
@@ -100,7 +100,14 @@ int DataStructureTester<L>::DoSequentialFind(int start, int end, int step) {
 
 template<class L>
 int DataStructureTester<L>::DoRandomFind(int n) {
-
+	t_ = clock();
+	//srand(0);
+	for (int i = 0; i < n; i++) {
+		int x = rand();
+		list.find(x);
+	}
+	t_ = clock() - t_;
+	return (float) t_ / CLOCKS_PER_SEC * 1000;
 }
 }
 
