d3fb1adfb902bb2bc92e82f1b8da36b1b5459eee
Irina Rakhlenko <rakhlei@miamioh.edu>
2014-11-16 18:36:58 -0500

    Add DoSequentialFind method

--- workspace/rakhlei_hw_5/src/DataStructureTester.h
+++ workspace/rakhlei_hw_5/src/DataStructureTester.h
@@ -90,7 +90,12 @@ int DataStructureTester<L>::DoRandomRemove(int n) {
 
 template<class L>
 int DataStructureTester<L>::DoSequentialFind(int start, int end, int step) {
-
+	t_ = clock();
+	for (int i = start; i <= end; i += step) {
+		list.find(i);
+	}
+	t_ = clock() - t_;
+	return (float) t_ / CLOCKS_PER_SEC * 1000;
 }
 
 template<class L>
